aa562d145414dc08cfc5d253154767451cdc26d1
Yuno835 <parryroger2004@gmail.com>
2026-07-30 20:04:08 +0630

    update firebase config for render

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,7 +1,15 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
 
-// အခုဆိုရင် admin.credential ဆိုတာ ရှိနေပါပြီ
+let serviceAccount;
+
+// Render ပေါ်မှာဆိုရင် Environment Variable ကနေဖတ်မယ်၊ 
+// ကိုယ့်စက် (Local) မှာဆိုရင် ဖိုင်ကနေ ယူသုံးမယ်
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+  serviceAccount = require('./firebase-service-account.json');
+}
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
